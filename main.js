5e91c260bb2d6ef779a2c693d8813acdaf08acdf
VaibhavPage <vaibhav.page@gmail.com>
2016-07-28 21:16:50 -0400

    first commit

--- main.js
+++ main.js
@@ -6,14 +6,13 @@ var app = electron.app;
 // Broweser window
 var BrowserWindow = electron.BrowserWindow;
 
-
 var window;
 
 function createWindow(){
 
     window = new BrowserWindow({width : 800, height : 800, title: "Notes"});    
     window.loadURL(`file://${__dirname}/index.html`);
-    window.webContents.openDevTools();
+  //  window.webContents.openDevTools();
     window.on('close', function() {
         window = null;
     });
